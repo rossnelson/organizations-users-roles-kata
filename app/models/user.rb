87481883ba41c98ddc9f892d61b4f041e69b605b
Ross <axcess1@me.com>
2014-11-29 19:32:15 -0500

    Remove trailing whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :email, 
-    presence: true, 
-    uniqueness: true, 
+  validates :email,
+    presence: true,
+    uniqueness: true,
     format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
   validates :password, presence: true, on: :create
 
